d1a2985fcd062ded19cc0269732516b21fe2ae22
Luke Campbell <2560897C@student.gla.ac.uk>
2023-01-30 18:03:51 +0000

    first commit in a while

---
+++ rango/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from rango import views
+
+app_name = 'rango'
+
+urlpatterns = [
+    path('/', views.index, name= 'index'),
+]
+
+
---
+++ rango/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return  render(request, 'rango/index.html', context=context_dict)
+
+
