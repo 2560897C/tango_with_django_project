0d9a1bcd9e8e24e742b1b2c4faa78f668d087c0c
Luke Campbell <2560897C@student.gla.ac.uk>
2023-02-02 17:17:51 +0000

    after chapter 5 commmit

--- populate_rango.py
+++ populate_rango.py
@@ -34,9 +34,9 @@ def populate():
             'Other Frameworks': {'pages': other_pages} }
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data['views'], cat_data['likes'])
         for p in cat_data['pages']:
-            add_page(c, p['title'], ['url'])
+            add_page(c, p['title'], p['url'], p['views'])
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
@@ -49,8 +49,8 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
-    c =  Category.objects.get_or_create(name=name)[0]
+def add_cat(name, views, likes):
+    c =  Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
     c.save()
     return c
 
--- rango/admin.py
+++ rango/admin.py
@@ -4,5 +4,12 @@ from rango.models import Category, Page
 
 # Register your models here.
 
+
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+
+
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
